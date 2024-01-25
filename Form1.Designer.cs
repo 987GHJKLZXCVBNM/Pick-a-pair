0065dd5c9e83e11dd071e5310d59cfe651060e7e
Retro6790 <114397005+987GHJKLZXCVBNM@users.noreply.github.com>
2024-01-25 13:56:54 +0500

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,6 +29,7 @@ namespace Подбери_пару
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.label16 = new System.Windows.Forms.Label();
             this.label15 = new System.Windows.Forms.Label();
@@ -46,6 +47,7 @@ namespace Подбери_пару
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -97,6 +99,7 @@ namespace Подбери_пару
             this.label16.Text = "c";
             this.label16.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label16.UseCompatibleTextRendering = true;
+            this.label16.Click += new System.EventHandler(this.label16_Click);
             // 
             // label15
             // 
@@ -106,9 +109,10 @@ namespace Подбери_пару
             this.label15.Name = "label15";
             this.label15.Size = new System.Drawing.Size(148, 139);
             this.label15.TabIndex = 14;
-            this.label15.Text = "c";
+            this.label15.Text = "T";
             this.label15.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label15.UseCompatibleTextRendering = true;
+            this.label15.Click += new System.EventHandler(this.label15_Click);
             // 
             // label14
             // 
@@ -121,6 +125,7 @@ namespace Подбери_пару
             this.label14.Text = "c";
             this.label14.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label14.UseCompatibleTextRendering = true;
+            this.label14.Click += new System.EventHandler(this.label14_Click);
             // 
             // label13
             // 
@@ -130,9 +135,10 @@ namespace Подбери_пару
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(133, 139);
             this.label13.TabIndex = 12;
-            this.label13.Text = "c";
+            this.label13.Text = "X";
             this.label13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label13.UseCompatibleTextRendering = true;
+            this.label13.Click += new System.EventHandler(this.label13_Click);
             // 
             // label12
             // 
@@ -142,9 +148,10 @@ namespace Подбери_пару
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(138, 137);
             this.label12.TabIndex = 11;
-            this.label12.Text = "c";
+            this.label12.Text = "0";
             this.label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label12.UseCompatibleTextRendering = true;
+            this.label12.Click += new System.EventHandler(this.label12_Click);
             // 
             // label11
             // 
@@ -154,9 +161,10 @@ namespace Подбери_пару
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(148, 137);
             this.label11.TabIndex = 10;
-            this.label11.Text = "c";
+            this.label11.Text = "Y";
             this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label11.UseCompatibleTextRendering = true;
+            this.label11.Click += new System.EventHandler(this.label11_Click);
             // 
             // label5
             // 
@@ -166,9 +174,10 @@ namespace Подбери_пару
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(133, 125);
             this.label5.TabIndex = 4;
-            this.label5.Text = "c";
+            this.label5.Text = "N";
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label5.UseCompatibleTextRendering = true;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // label4
             // 
@@ -178,9 +187,10 @@ namespace Подбери_пару
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(133, 125);
             this.label4.TabIndex = 3;
-            this.label4.Text = "c";
+            this.label4.Text = "m";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label4.UseCompatibleTextRendering = true;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // label3
             // 
@@ -190,9 +200,10 @@ namespace Подбери_пару
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(148, 125);
             this.label3.TabIndex = 2;
-            this.label3.Text = "c";
+            this.label3.Text = "m";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label3.UseCompatibleTextRendering = true;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label2
             // 
@@ -202,9 +213,10 @@ namespace Подбери_пару
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(138, 125);
             this.label2.TabIndex = 1;
-            this.label2.Text = "c";
+            this.label2.Text = "!";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label2.UseCompatibleTextRendering = true;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label1
             // 
@@ -214,7 +226,7 @@ namespace Подбери_пару
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(133, 137);
             this.label1.TabIndex = 0;
-            this.label1.Text = "c";
+            this.label1.Text = "!";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label1.UseCompatibleTextRendering = true;
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -227,9 +239,10 @@ namespace Подбери_пару
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(133, 137);
             this.label6.TabIndex = 5;
-            this.label6.Text = "c";
+            this.label6.Text = "N";
             this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label6.UseCompatibleTextRendering = true;
+            this.label6.Click += new System.EventHandler(this.label6_Click);
             // 
             // label7
             // 
@@ -239,9 +252,10 @@ namespace Подбери_пару
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(148, 137);
             this.label7.TabIndex = 6;
-            this.label7.Text = "c";
+            this.label7.Text = "T";
             this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label7.UseCompatibleTextRendering = true;
+            this.label7.Click += new System.EventHandler(this.label7_Click);
             // 
             // label8
             // 
@@ -251,9 +265,10 @@ namespace Подбери_пару
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(138, 137);
             this.label8.TabIndex = 7;
-            this.label8.Text = "c";
+            this.label8.Text = "Y";
             this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label8.UseCompatibleTextRendering = true;
+            this.label8.Click += new System.EventHandler(this.label8_Click);
             // 
             // label9
             // 
@@ -263,9 +278,10 @@ namespace Подбери_пару
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(133, 137);
             this.label9.TabIndex = 8;
-            this.label9.Text = "c";
+            this.label9.Text = "0";
             this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label9.UseCompatibleTextRendering = true;
+            this.label9.Click += new System.EventHandler(this.label9_Click);
             // 
             // label10
             // 
@@ -275,9 +291,15 @@ namespace Подбери_пару
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(133, 137);
             this.label10.TabIndex = 9;
-            this.label10.Text = "c";
+            this.label10.Text = "X";
             this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label10.UseCompatibleTextRendering = true;
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 750;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
@@ -311,6 +333,7 @@ namespace Подбери_пару
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
